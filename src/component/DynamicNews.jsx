9ee7567df19494d7418015499717232242b3ce5c
Sujoy chakraborty <sujoyc070@gmail.com>
2023-09-16 00:58:08 +0600

    news card

--- src/component/DynamicNews.jsx
+++ src/component/DynamicNews.jsx
@@ -1,19 +1,26 @@
 import Container from "react-bootstrap/Container";
+// import Card from "react-bootstrap/Card";
 import dynamicnews from "../dynamicNews/dynamicNews.json";
 
 const DynamicNews = () => {
   return (
     <>
-      <Container>
-        <div className="dynamic_news">
-          {dynamicnews.map((item) => (
-            <>
-              <h2>{item.category}</h2>
-              <p>{item.news}</p>
-            </>
-          ))}
-        </div>
-      </Container>
+      <div className="news_part p-4">
+        <Container>
+          <div className="dynamic_news">
+            <div className="main-card d-flex flex-wrap justify-content-between">
+              {dynamicnews.map((item) => (
+                <>
+                  <div className="card ">
+                    <h2>{item.category}</h2>
+                    <p>{item.news}</p>
+                  </div>
+                </>
+              ))}
+            </div>
+          </div>
+        </Container>
+      </div>
     </>
   );
 };
