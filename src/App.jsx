922cc8599c70287b42786651600c9805535c2bdb
Sujoy chakraborty <sujoyc070@gmail.com>
2023-09-28 13:14:33 +0600

    home upper

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
+import { useEffect, useState } from "react";
 import { Container, Col, Row } from "react-bootstrap";
 
 function App() {
+  let [trending, setTrending] = useState([]);
+
+  useEffect(() => {
+    const getNews = async () => {
+      const res = await fetch("/dynamicNews.json");
+      let getRecentnews = await res.json();
+      let newsData = getRecentnews.filter((item) => item.cat == "recent");
+      setTrending(newsData);
+    };
+    getNews();
+  }, []);
+  console.log(trending);
+
   return (
     <>
       <div className="home_page">
         <Container fluid>
-          <Row>
-            <Col xs={6}>
-              <div
-                className="check bg-primary "
-                style={{ height: "100px" }}></div>
-            </Col>
-            <Col xs={6}>
-              <div
-                className="check bg-danger "
-                style={{ height: "100px" }}></div>
-            </Col>
-          </Row>
+          <div className="upper_news">
+            <Row>
+              {trending.map((nitem) => (
+                <>
+                  <Col xs={6}>
+                    <h2 className="news_title">{nitem.title}</h2>
+                    <p>{nitem.news}</p>
+                    <div className="img">
+                      <img
+                        src={nitem.img}
+                        alt=""
+                      />
+                    </div>
+                  </Col>
+                </>
+              ))}
+            </Row>
+          </div>
         </Container>
       </div>
     </>
