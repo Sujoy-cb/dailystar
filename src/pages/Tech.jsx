db8d60ea4cc7f46f110bc84b6647dccc6fa05a0c
Sujoy chakraborty <sujoyc070@gmail.com>
2023-10-02 23:18:41 +0600

    tech news update

--- src/pages/Tech.jsx
+++ src/pages/Tech.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import Title from "../component/Title/Title";
 
 const Tech = () => {
   let [technews, setTechnews] = useState([]);
   let [technews2, setTechnews2] = useState([]);
+  let [technews3, setTechnews3] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
@@ -22,6 +23,15 @@ const Tech = () => {
       const news = await response.json();
       let newsData = news.filter((item) => item.cat == "lefttech");
       setTechnews2(newsData);
+    };
+    getData();
+  }, []);
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("/dynamicNews.json");
+      const news = await response.json();
+      let newsData = news.filter((item) => item.cat == "tech2");
+      setTechnews3(newsData);
     };
     getData();
   }, []);
@@ -43,11 +53,43 @@ const Tech = () => {
                     alt=""
                   />
                 </div>
-                <p className="mt-2">{item.news}</p>
+                <p className="left_news mt-2">{item.news}</p>
               </>
             ))}
           </Col>
-          <Col xs={6}></Col>
+          <Col xs={6}>
+          <Row>
+            <Col xs={6}>
+            {technews.map((item) => (
+              <>
+                <h3 className="mt-4">{item.title}</h3>
+                <div className="techimg mt-4">
+                  <img
+                    src={item.img}
+                    alt=""
+                  />
+                </div>
+                <p className="mt-2">{item.news}</p>
+              </>
+            ))}
+            </Col>
+            <Col xs={6}>
+            {technews3.map((item) => (
+              <>
+                <h3 className="mt-4">{item.title}</h3>
+                <div className="techimg mt-4">
+                  <img
+                    src={item.img}
+                    alt=""
+                  />
+                </div>
+                <p className="mt-2">{item.news}</p>
+              </>
+            ))}
+            </Col>
+          </Row>
+          
+          </Col>
         </Row>
       </div>
     </>
